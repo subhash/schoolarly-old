cf410677c558a431edd92217656e834fcd5c1df7
shenuja <shenuja@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2011-01-06 11:36:45 +0000

    student input

--- app/controllers/member/students_controller.rb
+++ app/controllers/member/students_controller.rb
@@ -1,14 +1,24 @@
 class Member::StudentsController < ApplicationController
   
+  require "csv"
+  
+  def new
+    @school = School.find(params[:school_id])
+  end
+  
   def create
     @school = School.find(params[:school_id])
-    @user = User.new(:email => params[:email])    
-    @user.login ||= @user.email if Tog::Config["plugins.tog_user.email_as_login"]
-    @user.profile = Profile.new(:first_name => params[:name])    
-    @user.person = Student.new
-    @user.invite_over_email 
-    # TODO check if you are allowed to invite
-    @school.group.invite_and_accept(@user)
+    CSV.parse(params[:students]) do |row|
+      @user = User.new(:email => row[1])
+      @user.login ||= @user.email if Tog::Config["plugins.tog_user.email_as_login"]
+      name = row[0].split(' ',2)
+      @user.profile = Profile.new(:first_name => name[0],:last_name => name[1])
+      @user.person = Student.new
+      @user.invite_over_email 
+      
+      # TODO check if you are allowed to invite
+      @school.group.invite_and_accept(@user)
+    end
     redirect_back_or_default(Tog::Config["plugins.tog_user.default_redirect_on_signup"])
     flash[:notice] = I18n.t("tog_user.user.sign_up")
   end
