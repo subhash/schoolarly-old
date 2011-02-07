924bff8c4faf05027fe9c6cda1ba4307987cd63d
subhash <subhashgo@gmail.com>
2011-02-07 14:46:17 +0530

    Redirecting to My Groups after login and activation

--- config/environment.rb
+++ config/environment.rb
@@ -29,31 +29,31 @@ Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
-
+  
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
+  
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
+  
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-
+  
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
+  
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'UTC'
-
+  
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
@@ -66,6 +66,16 @@ Tog::Interface.sections(:site).add :groups, "/groups"
 Tog::Interface.sections(:site).add :blogs, "/blogs"
 Tog::Interface.sections(:site).add :events, "/conclave/events"
 
-Tog::Config['plugins.tog_user.email_as_login'] = true
-Tog::Config['plugins.tog_core.site.name'] = "Schoolarly"
-Tog::Plugins.settings :tog_social, "profile.image.versions.tiny"   => "35x35#"
+Tog::Plugins.settings :tog_core, {"site.name" => "Schoolarly"}, :force => true
+
+Tog::Plugins.settings :tog_social, {"profile.image.versions.tiny"   => "35x35#"}, :force => true
+
+Tog::Plugins.settings :tog_user,  {
+    :email_as_login                   => true, 
+    :default_redirect_on_login        => "/member/groups",
+    :default_redirect_on_logout       => "/",
+    :default_redirect_on_signup       => "/",
+    :default_redirect_on_activation   => "/member/groups",
+    :default_redirect_on_forgot       => "/",
+    :default_redirect_on_reset        => "/"
+  }, :force => true
