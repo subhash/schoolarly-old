2376c822a8ffc25caf6f11b34811eaa971fcafbe
subhash <subhashgo@gmail.com>
2011-03-20 10:22:16 +0530

    Calendar first version

--- app/models/event.rb
+++ app/models/event.rb
@@ -26,5 +26,8 @@
 #  moderated         :boolean
 #
 class Event < ActiveRecord::Base
+
+  named_scope :between, lambda { |start_date, end_date| { :conditions => ['start_date >= ? and end_date <= ?', start_date, end_date], :order => 'start_date desc, start_time desc' } }  
+  
   acts_as_commentable
 end
