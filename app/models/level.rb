13a307b05f6777936808dfd859293f3e4ecd03dd
subhash <subhashgo@gmail.com>
2011-04-02 15:37:38 +0530

    Deleting levels and criteria from the rubric table

--- app/models/level.rb
+++ app/models/level.rb
@@ -3,6 +3,6 @@ class Level < ActiveRecord::Base
   belongs_to :rubric
   acts_as_list :scope => :rubric
 
-  has_many :rubric_descriptors
+  has_many :rubric_descriptors, :dependent => :destroy
   
 end
