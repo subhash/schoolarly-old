7cbd87fee41321a438d84bf278f9d91d6afeecc8
subhash <subhashgo@gmail.com>
2011-04-25 12:14:59 +0530

    Fixing paginate with filtering dashboard

--- app/controllers/member/dashboard_controller.rb
+++ app/controllers/member/dashboard_controller.rb
@@ -5,9 +5,24 @@ class Member::DashboardController < Member::BaseController
     @order = params[:order] || 'updated_at'
     @page = params[:page] || '1'
     @asc = params[:asc] || 'desc'
-    @shares = Share.shared_to_groups(current_user.group_ids).paginate  :per_page => 10,
-                                                :page => @page,
-                                                :order => "#{@order} #{@asc}"  
+    @filter = params[:filter] || 'All'
+    if @filter == 'All'
+      @shares = Share.shared_to_groups(current_user.group_ids).paginate  :per_page => 10,
+                                                  :page => @page,
+                                                  :order => "#{@order} #{@asc}"
+    else
+      @shares = Share.shared_to_groups_of_type(@profile.user.group_ids, @filter).paginate :per_page => 10,
+                                           :page => @page, 
+                                           :order => "updated_at desc" 
+    end
+    respond_to do |wants|
+      wants.html
+      wants.js do
+        render :update do |page|
+          page.replace_html 'sharings', :partial => 'member/sharings/sharings'
+        end
+      end
+    end
   end
   
 end
