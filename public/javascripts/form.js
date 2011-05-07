0c8e06549bb0348879efb07bd162ed6cac8c1e46
shenuja <shenuja@gmail.com>
2011-05-07 15:24:57 +0530

    css changes

---
+++ public/javascripts/form.js
@@ -0,0 +1,12 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+jQuery.noConflict();
+
+jQuery(document).ready(function(){
+    jQuery('.fields *').focus(function(){
+        jQuery(this).parents('p').addClass('focused');
+    }).blur(function(){
+        jQuery(this).parents('p').removeClass('focused');
+    });
+});
