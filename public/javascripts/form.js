6d29972a07b77c60600725e5b2eb9d8c42480e9f
shenuja <shenuja@gmail.com>
2011-05-09 14:39:13 +0530

    highlight for events too... moving js to the specific forms

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -4,10 +4,11 @@
 jQuery.noConflict();
 
 jQuery(document).ready(function(){
-
-    jQuery('.fields *').focus(function(){
+    jQuery('form .fields *').focus(function(){
         jQuery(this).parents('p').addClass('focused');
+		jQuery(this).parents('tr').addClass('focused');
     }).blur(function(){
         jQuery(this).parents('p').removeClass('focused');
+		jQuery(this).parents('tr').removeClass('focused');
     });
 });
