1f33a95794a1d70bbe78bfe3b2413bd1812bc7b1
shenuja <shenuja@gmail.com>
2011-06-03 15:02:47 +0530

    some details - adding due date

--- app/helpers/conclave/events_helper.rb
+++ app/helpers/conclave/events_helper.rb
@@ -5,5 +5,23 @@ module Conclave
       event.owner == current_user
     end
     
+    def write_event_date_time(event, format=:short)
+      if (event.start_date != event.end_date)
+        write_event_date(event)
+      else
+        write_event_time(event)
+      end
+      
+    end
+    
+    def write_event_date_time_detail(event, format=:short)
+      if (event.start_date != event.end_date)
+        "#{I18n.t('tog_conclave.site.from_date')}: #{event.starting_date(format)}, #{event.starting_time} #{I18n.t('tog_conclave.site.to_date')}: #{event.ending_date(format)}, #{event.ending_time}"
+      else
+        write_event_time(event)
+      end
+      
+    end
+    
   end
 end
