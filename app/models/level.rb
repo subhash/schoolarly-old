e013b8f092f6c45688da769a69ba9f76a4518e74
shenuja <shenuja@gmail.com>
2011-06-09 11:34:36 +0530

    fixing bugs abt points

--- app/models/level.rb
+++ app/models/level.rb
@@ -2,7 +2,7 @@ class Level < ActiveRecord::Base
   
   belongs_to :rubric
   acts_as_list :scope => :rubric
-
+  
   has_many :rubric_descriptors, :dependent => :destroy
   
   validates_presence_of :name
