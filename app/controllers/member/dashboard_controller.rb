cfa534e93e1b3dc1f704102df2f21063d1ebfe19
subhash <subhashgo@gmail.com>
2011-06-23 20:33:50 +0530

    Parents can see shares to wards

--- app/controllers/member/dashboard_controller.rb
+++ app/controllers/member/dashboard_controller.rb
@@ -7,15 +7,17 @@ class Member::DashboardController < Member::BaseController
     @asc = params[:asc] || 'desc'
     @filter = params[:filter] || 'All'
     group_ids = current_user.group_ids
+    user_ids = [current_user.id]
     if current_user.parent?
       group_ids += current_user.friend_users.inject([]) {|c, u| c += u.group_ids}
-    end
+      user_ids += current_user.friend_users.map(&:id)
+    end     
     if @filter == 'All'
-      @shares = Share.to_groups_and_user(group_ids,current_user.id).paginate  :per_page => 10,
+      @shares = Share.to_groups_and_users(group_ids, user_ids).paginate  :per_page => 10,
                                                   :page => @page,
                                                   :order => "#{@order} #{@asc}"
     else
-      @shares = Share.to_groups_and_user_of_type(group_ids, current_user.id, @filter).paginate :per_page => 10,
+      @shares = Share.to_groups_and_users_of_type(group_ids, user_ids, @filter).paginate :per_page => 10,
                                            :page => @page, 
                                            :order => "#{@order} #{@asc}" 
     end
