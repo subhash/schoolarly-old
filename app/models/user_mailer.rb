580714bdba61bedab6ed3bcf114d168a2e353c45
shenuja <shenuja@gmail.com>
2011-10-21 14:57:12 +0530

    Modifying invite email

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   def signup_invitation_notification(user, inviter)
     setup_email(user)
     @from                   = "#{inviter.name} <#{inviter.email}>"
-    @subject                += "Invitation from #{inviter.name} (#{inviter.school.group.name})" 
+    @subject                += "Invitation from #{inviter.name}" 
     @body[:url]             = reset_url(user.password_reset_code)
     @body[:inviter]         = inviter
   end
