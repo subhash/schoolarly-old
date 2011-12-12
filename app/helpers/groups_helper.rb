0db44ae0d0d65dc9856a05e506617e722f9373a0
shenuja <shenuja@gmail.com>
2011-12-12 12:00:35 +0530

    including form effects fixing ancestor_path bug

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,8 +1,9 @@
 module GroupsHelper
   def ancestor_path_for(group)
+    #    have to take group.parent & cannot do group.ancestors 'coz group is a New group(not saved into db)
     return unless group.parent
-      s = group.parent.self_and_ancestors.reverse.map{|g| link_to(g.name, (current_user ? member_group_path(g) : group_path(g))) }.join " > " 
-      return s + " > "
+    s = group.parent.self_and_ancestors.reverse.map{|g| link_to(g.name, (current_user ? member_group_path(g) : group_path(g))) }.join " > " 
+    return s + " > "
   end
   
   def path_for(group)
