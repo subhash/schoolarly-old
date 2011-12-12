5116ad20c1342cd59304cab4391cefde57e416a9
shenuja <shenuja@gmail.com>
2011-12-12 19:38:37 +0530

    removing new_relic and pg

--- config/environment.rb
+++ config/environment.rb
@@ -43,7 +43,6 @@ Rails::Initializer.run do |config|
 
   config.gem 'ice_cube'
   config.gem 'chronic'
-  config.gem 'newrelic_rpm'
   
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
