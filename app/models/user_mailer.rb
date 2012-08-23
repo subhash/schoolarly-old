4ceaeba135387986b361fcfc182305c74957f97d
subhash <subhashgo@gmail.com>
2012-08-23 09:48:25 +0530

    Hardcoding mail subject for GPS

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   def signup_invitation_notification(user, inviter)
     setup_email(user)
     @from                   = "#{inviter.name} <#{inviter.email}>"
-    @subject                += "Invitation from #{inviter.name}" 
+    @subject                += "Invitation from Global Public School - Welcome to Schoolarly Network" 
     @body[:url]             = reset_url(user.password_reset_code)
     @body[:inviter]         = inviter
   end
