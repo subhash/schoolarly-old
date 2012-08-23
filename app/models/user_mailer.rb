95c42f03ebcc82ff3af786d9b1485dd1dd1d914c
shenuja <shenuja@gmail.com>
2012-08-23 19:26:04 +0530

    Changing signup email format

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,10 +3,13 @@ class UserMailer < ActionMailer::Base
   def signup_invitation_notification(user, inviter)
     setup_email(user)
     @from                   = "#{inviter.name} <#{inviter.email}>"
-    @subject                += "Invitation from Global Public School - Welcome to Schoolarly Network" 
+    @subject                += user.school ? "Invitation from #{user.school} - Welcome to Schoolarly Network" : "Welcome to Schoolarly Network"
     @body[:url]             = reset_url(user.password_reset_code)
-    @body[:inviter]         = inviter
+    @body[:inviter]         = inviter.school ? "IT team @ #{inviter.school}" : inviter.name
+    @body[:school]          = inviter.school ? inviter.school : "Schoolarly team"
+    @body[:parent_text]     = inviter.parent? ? "Be in touch with your child's school activities!" : ""
+    @body[:address]         = inviter.parent? ? "Dear Parent/Guardian" : "Hi"
   end
-
-
+  
+  
 end
