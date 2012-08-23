87ce8b94d9a56b2d323c63024521f606023dbec2
shenuja <shenuja@gmail.com>
2012-08-23 19:36:19 +0530

    fixing bug with school name

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,12 +3,12 @@ class UserMailer < ActionMailer::Base
   def signup_invitation_notification(user, inviter)
     setup_email(user)
     @from                   = "#{inviter.name} <#{inviter.email}>"
-    @subject                += user.school ? "Invitation from #{user.school} - Welcome to Schoolarly Network" : "Welcome to Schoolarly Network"
+    @subject                += user.school ? "Invitation from #{user.school.group.name} - Welcome to Schoolarly Network" : "Welcome to Schoolarly Network"
     @body[:url]             = reset_url(user.password_reset_code)
-    @body[:inviter]         = inviter.school ? "IT team @ #{inviter.school}" : inviter.name
-    @body[:school]          = inviter.school ? inviter.school : "Schoolarly team"
-    @body[:parent_text]     = inviter.parent? ? "Be in touch with your child's school activities!" : ""
-    @body[:address]         = inviter.parent? ? "Dear Parent/Guardian" : "Hi"
+    @body[:inviter]         = inviter.school ? "IT team @ #{inviter.school.group.name}" : inviter.name
+    @body[:school]          = inviter.school ? inviter.school.group.name : "Schoolarly team"
+    @body[:parent_text]     = user.parent? ? "Be in touch with your child's school activities!" : ""
+    @body[:address]         = user.parent? ? "Dear Parent/Guardian" : "Hi"
   end
   
   
