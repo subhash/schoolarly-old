eb391cd1858a27b40740912645302288c3c0ffbc
shenuja <shenuja@gmail.com>
2012-08-23 21:00:04 +0530

    fixing error

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   def signup_invitation_notification(user, inviter)
     setup_email(user)
     @from                   = "#{inviter.name} <#{inviter.email}>"
-    @subject                += user.school ? "Invitation from #{user.school.group.name} - Welcome to Schoolarly Network" : "Welcome to Schoolarly Network"
+    @subject                += inviter.school ? "Invitation from #{inviter.school.group.name} - Welcome to Schoolarly Network" : "Welcome to Schoolarly Network"
     @body[:url]             = reset_url(user.password_reset_code)
     @body[:inviter]         = inviter.school ? "IT team @ #{inviter.school.group.name}" : inviter.name
     @body[:school]          = inviter.school ? inviter.school.group.name : "Schoolarly team"
