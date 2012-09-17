5cccac0b9aefd25788ea1f233fc802ac961e6ac1
Subhash Gopalakrishnan <subhashgo@gmail.com>
2012-09-17 10:54:25 +0530

    Including group id in ref so that we can share to group once video is uploaded

--- app/controllers/member/videos_controller.rb
+++ app/controllers/member/videos_controller.rb
@@ -1,15 +1,15 @@
 class Member::VideosController < Member::BaseController
   before_filter :find_group, :except => [:index, :destroy]
   before_filter :find_video, :except => [:new, :create, :index, :youtube_update]
-  
   def create
     @video = Video.new(params[:video])
     @video.user = current_user
     if(params[:upload])
       @video.save(false)
       yt = Video.youtube_client
+      ref_id = "#{@video.id}" + (@group ? "_#{@group.id}" : "")
       @upload_info = yt.upload_token({:title => @video.title, :description => @video.description,
-        :keywords => %w[Schoolarly], :category => 'People', :unlisted => true}, youtube_update_member_videos_url(:video_id => @video.id))
+        :keywords => %w[Schoolarly], :category => 'People', :list => 'denied'}, youtube_update_member_videos_url(:ref_id => ref_id))
       @video.token = @upload_info[:token]
     end
     respond_to do |wants|
@@ -18,7 +18,7 @@ class Member::VideosController < Member::BaseController
           if @video.token.blank?
             @group.share(current_user, @video.class.to_s, @video.id) if @group
             flash[:ok] = I18n.t('videos.member.add_success')
-            redirect_to member_video_path(@video, :group => @group)
+            redirect_to @group ? member_group_path(@group) : member_video_path(@video, :group => @group)
           else
             flash[:ok] = "Pick video file"
             render 'new'
@@ -46,13 +46,15 @@ class Member::VideosController < Member::BaseController
   end
 
   def youtube_update
-    @video = Video.find(params[:video_id])
+    video_id, group_id = params[:ref_id].split('_')
+    @video = Video.find(video_id)
+    @group = Group.find(group_id) if group_id
     @video.link = "http://www.youtube.com/watch?v=#{params[:id]}"
     @video.token = nil
     if @video.save
       @group.share(current_user, @video.class.to_s, @video.id) if @group
       flash[:ok] = I18n.t('videos.member.add_success')
-      redirect_to member_video_path(@video, :group => @group)
+      redirect_to @group ? member_group_path(@group) : member_video_path(@video, :group => @group)
     else
       flash[:error] = I18n.t('videos.member.add_failure')
       render :new
