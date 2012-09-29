72132167af54a3a27c38473f371aa865f05b197d
shenuja <shenuja@gmail.com>
2012-09-29 10:43:10 +0530

    1) Add Export as CSV feature to tables 2) Changing table view of school group, adding classes column 3) Some style changes to group show

--- app/controllers/member/profiles_controller.rb
+++ app/controllers/member/profiles_controller.rb
@@ -30,23 +30,60 @@ class Member::ProfilesController < Member::BaseController
   end
   
   
+  #  def index    
+  #    if params[:group]
+  #      @group = Group.find(params[:group])
+  #      @can_view_email = current_user.can_view_email?(@group)
+  #      if params[:type] == 'Parent'
+  #        @column_groups = []
+  #        @users = @group.parent_users
+  #      else
+  #        @column_groups = @group.school? ? [] : @group.active_children  
+  #        @users = @group.users.of_type(params[:type])
+  #        @memberships = Membership.find(:all, :conditions => {:user_id => @users.map(&:id), :group_id => @column_groups.map(&:id)}).group_by(&:user_id)
+  #        @memberships.each do |key, value|
+  #          h = {}
+  #          value.each{|m| h[m.group_id] = m}
+  #          @memberships[key] = h
+  #        end
+  #      end    
+  #      @type = @users.first.type
+  #    else
+  #      @users = User.all
+  #      @column_groups = []
+  #    end 
+  #    respond_to do |format|
+  #      format.html # index.html.erb
+  #      format.xml  { render :xml => @profiles }
+  #    end
+  #  end
+  
+  
   def index    
     if params[:group]
       @group = Group.find(params[:group])
       @can_view_email = current_user.can_view_email?(@group)
       if params[:type] == 'Parent'
-        @column_groups = []
         @users = @group.parent_users
-      else
-        @column_groups = @group.school? ? [] : @group.active_children  
+      else 
         @users = @group.users.of_type(params[:type])
-        @memberships = Membership.find(:all, :conditions => {:user_id => @users.map(&:id), :group_id => @column_groups.map(&:id)}).group_by(&:user_id)
-        @memberships.each do |key, value|
-          h = {}
-          value.each{|m| h[m.group_id] = m}
-          @memberships[key] = h
-        end
-      end    
+      end
+      if (@group.school? && !(params[:type] == 'Parent'))
+        klass_groups = @group.active_children.klass
+        @memberships  = Membership.find(:all, :include => :group, :conditions => {:user_id => @users.map(&:id), :group_id => klass_groups.map(&:id)}).group_by(&:user_id)
+      else
+        if params[:type] == 'Parent'
+          @column_groups = []
+        else
+          @column_groups = @group.active_children  
+          @memberships = Membership.find(:all, :conditions => {:user_id => @users.map(&:id), :group_id => @column_groups.map(&:id)}).group_by(&:user_id)
+          @memberships.each do |key, value|
+            h = {}
+            value.each{|m| h[m.group_id] = m}
+            @memberships[key] = h
+          end
+        end 
+      end
       @type = @users.first.type
     else
       @users = User.all
@@ -57,6 +94,7 @@ class Member::ProfilesController < Member::BaseController
       format.xml  { render :xml => @profiles }
     end
   end
+  
   
   def edit
   end
