8d469df69ee3aee7687428db61129549d3b620e0
Subhash Gopalakrishnan <subhashgo@gmail.com>
2013-01-03 18:56:14 +0530

    Undoing dashboard debug

--- app/controllers/member/dashboard_controller.rb
+++ app/controllers/member/dashboard_controller.rb
@@ -11,41 +11,28 @@ class Member::DashboardController < Member::BaseController
     @filter = params[:filter] || 'All'
     group_ids = current_user.group_ids
     user_ids = [current_user.id]
-    puts "before current_user.parent?"
     if current_user.parent?
       group_ids += current_user.friend_users.inject([]) {|c, u| c += u.group_ids}
       user_ids += current_user.friend_users.map(&:id)
     end
-    puts "after current_user.parent?"
-    puts "before block"
     if current_user.admin?
       group_ids = Group.all.collect(&:id)
     elsif current_user.school_admin?
       group_ids += current_user.school.group.descendants.collect(&:id)
     else
-      puts "inside block - else "
       group_ids += current_user.moderated_groups.block.collect{|b|b.descendants.collect(&:id)}.flatten
     end     
-    puts "outside block"
     if @filter == 'All'
-      puts 'before shares'
       @shares = Share.to_groups_and_users(group_ids, user_ids).paginate  :per_page => 20,
                                                   :page => @page,
                                                   :order => "#{@order} #{@asc}"
-      puts 'after shares'
     else
       @shares = Share.to_groups_and_users_of_type(group_ids, user_ids, @filter).paginate :per_page => 20,
                                            :page => @page, 
                                            :order => "#{@order} #{@asc}" 
     end
     respond_to do |wants|
-      puts 'before rendering'
-      wants.html do
-        if current_user.parent?
-          render 'parent_index'
-        end
-      end
-      puts 'after rendering'
+      wants.html
       wants.js do
         render :update do |page|
           page.replace_html 'sharings', :partial => 'member/sharings/sharings'
