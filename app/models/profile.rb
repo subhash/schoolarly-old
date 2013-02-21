581dad2b838657dbd12e48e3fb3f05620d0c0126
Subhash Gopalakrishnan <subhashgo@gmail.com>
2013-02-21 14:51:12 +0530

    Prevent default icons for every user

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -34,8 +34,9 @@ class Profile < ActiveRecord::Base
       if FileTest.exist?(RAILS_ROOT + "/public/images/#{full_name}.jpg")
         self.icon = File.new(RAILS_ROOT + "/public/images/#{full_name}.jpg")
       elsif Tog::Config["plugins.tog_social.profile.image.default"]
-        default_profile_icon = File.join(RAILS_ROOT, 'public', 'tog_social', 'images', Tog::Config["plugins.tog_social.profile.image.default"])
-        self.icon = File.new(default_profile_icon)
+        # Prevent default icons for each user
+        # default_profile_icon = File.join(RAILS_ROOT, 'public', 'tog_social', 'images', Tog::Config["plugins.tog_social.profile.image.default"])
+        # self.icon = File.new(default_profile_icon)
       end
     end
   end
