1635f77d53ef82c0eadcbeab486c6a06c7c71927
shenuja <shenuja@gmail.com>
2013-06-17 19:06:54 +0530

    making photoset commentable to eager load comments for shares

--- app/models/picto/photoset.rb
+++ app/models/picto/photoset.rb
@@ -1,7 +1,7 @@
 class Picto::Photoset < ActiveRecord::Base
   
   acts_as_shareable
-  
+  acts_as_commentable  # added as dummy entry to make eager loading of comments possible in dashboard
   has_many :shares_to_groups, :class_name => 'Share', :as => :shareable, :conditions => {:shared_to_type => 'Group'}, :include => {:shared_to => [:parent] }
 
 end
