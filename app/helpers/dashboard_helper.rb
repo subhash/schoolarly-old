7b43897340201e7c54932b2749a058e345e3974c
shenuja <shenuja@gmail.com>
2013-06-17 20:21:19 +0530

    optimising config table fetch (& icons loading for each list_detail)

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -2,38 +2,38 @@ module DashboardHelper
   
   def icon_for_assignment(assignment, options={})
     if assignment.has_submissions
-      return image_tag("/images/#{config["plugins.schoolarly.homeactivity.image.default"]}" , options)
+      return image_tag("/images/dashboard/homeactivity2.png" , options)
     else
-      return image_tag("/images/#{config["plugins.schoolarly.classactivity.image.default"]}" , options)
+      return image_tag("/images/dashboard/classactivity.png" , options)
     end
   end    
   
   def icon_for_aggregation(options={})
-    return image_tag("/images/#{config["plugins.schoolarly.aggregation.image.default"]}" , options)
+    return image_tag("/images/dashboard/aggregation2.png" , options)
   end
   
   def icon_for_submission(options={})
-    return image_tag("/images/#{config["plugins.schoolarly.submission.image.default"]}" , options)
+    return image_tag("/images/dashboard/submission2.png" , options)
   end
   
   def icon_for_grade(options={})
-    return image_tag("/images/#{config["plugins.schoolarly.grade.image.default"]}" , options)
+    return image_tag("/images/dashboard/grade2.png" , options)
   end
   
   def icon_for_attachment(options={})
-    return image_tag("/images/#{config["plugins.schoolarly.attachment.image.default"]}" , options)
+    return image_tag("/images/dashboard/document2.png" , options)
   end
   
   def icon_for_post(options={})
-    return image_tag("/images/#{config["plugins.schoolarly.post.image.default"]}" , options)
+    return image_tag("/images/dashboard/note2.png" , options)
   end
   
   def icon_for_event(options={})
-    return image_tag("/images/#{config["plugins.schoolarly.event.image.default"]}" , options)
+    return image_tag("/images/dashboard/event2.png" , options)
   end
   
   def icon_for_notice(options={})
-    return image_tag("/images/#{config["plugins.schoolarly.notice.image.default"]}" , options)
+    return image_tag("/images/dashboard/notice2.png" , options)
   end
   
   def display?(share)
