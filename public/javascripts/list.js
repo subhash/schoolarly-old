50297b532a98d7fc8664951af2c23866f82f5b85
shenuja <shenuja@gmail.com>
2013-08-19 09:46:54 +0530

    taking latest filter which works on safari, & filter checkbox logic different

--- public/javascripts/list.js
+++ public/javascripts/list.js
@@ -2,40 +2,46 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 jQuery(document).ready(function(){
-	jQuery.noConflict();
+    jQuery.noConflict();
     applyListFilters();
 });
 
 function applyListFilters(){
     jQuery('#sharings').jplist({
+    
+        items_box: '.list',
+        item_path: '.list-item',
         
-            items_box: '.list',
-            item_path: '.list-item',
-            
-            redraw_callback: function(){
-            	    applyPrettyPhoto();
+        redraw_callback: function(){
+            applyPrettyPhoto();
+        },
+        
+        panel_path: '.panel' //cookies
+        ,
+        cookies: false,
+        expiration: -1 //cookies expiration in days (-1 = cookies expire when browser is closed)
+        ,
+        cookie_name: 'jplist-div-layout',
+        control_types: {
+            'reset': {
+                class_name: 'Reset',
+                options: {}
+            }            //CheckboxFilter - checkbox filter control 
+            ,
+            'checkbox-filters': {
+                class_name: 'CheckboxFilter',
+                options: {}
+            }            //CheckboxGroupFilter - group of checkboxes control 
+            ,
+            'checkbox-group-filter': {
+                class_name: 'CheckboxGroupFilter',
+                options: {}
             },
-            
-            panel_path: '.panel' //cookies
-            ,
-            cookies: false,
-            expiration: -1 //cookies expiration in days (-1 = cookies expire when browser is closed)
-            ,
-            cookie_name: 'jplist-div-layout',
-            control_types: {
-                'reset': {
-                    class_name: 'control_reset',
-                    options: {}
-                },
-                'cb_filters': {
-                    class_name: 'control_checkbox_filters',
-                    options: {}
-                },
-                'default_sort': {
-					class_name: 'control_default_sort',
-					options: {}
-				}
+            'default-sort': {
+                class_name: 'DefaultSort',
+                options: {}
             }
-        });
+        }
+    });
 }
 
